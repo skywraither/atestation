02864d52e2ff775f5f774f1f1672ce71e4fbebb0
Александр Кудрявцев <102574426+skywraither@users.noreply.github.com>
2023-07-25 14:36:03 +0300

    обработка входных данных

--- Program.cs
+++ Program.cs
@@ -10,3 +10,16 @@
         {
             Console.WriteLine(item);
         }
+
+
+        static string[] FilterArrayByLength(string[] inputArray)
+    {
+        int count = 0;
+        for (int i = 0; i < inputArray.Length; i++)
+        {
+            if (inputArray[i].Length <= 3)
+            {
+                count++;
+            }
+        }
+    }
