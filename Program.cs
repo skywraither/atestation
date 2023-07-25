bbeb286f19610945d1780f76e23c7f8739e97487
Александр Кудрявцев <102574426+skywraither@users.noreply.github.com>
2023-07-25 14:32:39 +0300

    ввод данных

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Введите элементы массива через запятую: ");
+        string input = Console.ReadLine();
+
+        string[] inputArray = input.Split(',');
+
+        string[] resultArray = FilterArrayByLength(inputArray);
